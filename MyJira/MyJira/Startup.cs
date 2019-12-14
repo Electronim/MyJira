36396cd244d01b60277644ebbeed7283f9b9b015
Electronim <petrascosandu@gmail.com>
2019-12-14 16:11:49 +0200

    test2

---
+++ MyJira/MyJira/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyJira.Startup))]
+namespace MyJira
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
