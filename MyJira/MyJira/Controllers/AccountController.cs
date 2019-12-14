537f0c139d59712924a8b5e669e8423aed1f960c
slym777 <darii.dan98@gmail.com>
2019-12-14 20:13:12 +0200

    Added CRUD on Teams and Users

--- MyJira/MyJira/Controllers/AccountController.cs
+++ MyJira/MyJira/Controllers/AccountController.cs
@@ -151,7 +151,7 @@ namespace MyJira.Controllers
         {
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                var user = new ApplicationUser { UserName = model.Email, Email = model.Email, TeamId = 1};
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
--- MyJira/MyJira/Models/IdentityModels.cs
+++ MyJira/MyJira/Models/IdentityModels.cs
@@ -12,10 +12,11 @@ namespace MyJira.Models
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
-        //[Required(ErrorMessage = "Team is mandatory")]
-        //public int TeamId { get; set; }
-        //public virtual Team Team { get; set; }
+        [Required(ErrorMessage = "Team is mandatory")]
+        public int TeamId { get; set; }
+        public virtual Team Team { get; set; }
         public IEnumerable<SelectListItem> AllRoles { get; set; }
+        public IEnumerable<SelectListItem> AllTeams { get; set; }
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
